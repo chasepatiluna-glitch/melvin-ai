a2b5c286d695686924c73ec8e49e55d9fced6c1b
chasepatiluna-glitch <chasepatiluna@gmail.com>
2026-05-16 23:41:46 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -2,34 +2,8 @@ export default async function handler(req, res) {
 
   try {
 
-    const message = req.body.message;
-
-    const response = await fetch(
-      "https://api.groq.com/openai/v1/chat/completions",
-      {
-        method: "POST",
-        headers: {
-          "Authorization": `Bearer ${process.env.GROQ_API_KEY}`,
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-          model: "llama3-8b-8192",
-          messages: [
-            {
-              role: "user",
-              content: message
-            }
-          ]
-        })
-      }
-    );
-
-    const data = await response.json();
-
-    console.log(data);
-
     return res.status(200).json({
-      reply: reply: JSON.stringify(data)
+      reply: "FREE AI CONNECTED SUCCESSFULLY"
     });
 
   } catch (err) {
