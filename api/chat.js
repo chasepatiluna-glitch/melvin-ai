a40095515c55b256541b12107a3a3696ac0b8523
chasepatiluna-glitch <chasepatiluna@gmail.com>
2026-05-16 23:25:00 +0800

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     return res.status(200).json({
-      reply: data.choices[0].message.content
+      reply: reply: JSON.stringify(data)
     });
 
   } catch (err) {
